892db92047794e0baa2cb71365fbefc6f2d26f21
TheWezard <43894956+TheWezard@users.noreply.github.com>
2018-11-07 17:37:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Dat
 $serverName = "tcp:sznoh.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-$result=$conn->query('SELECT * FROM Platnosci_SZNOH');
-$cols = $result->columnCount();
-echo 'Number of returned columns: '. $cols. '<br />';
+#$result=$conn->query('SELECT * FROM Platnosci_SZNOH');
+#$cols = $result->columnCount();
+#echo 'Number of returned columns: '. $cols. '<br />';
 echo "Helloo World!";
