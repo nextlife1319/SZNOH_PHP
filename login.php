a0187dcd407a06ef14ff719b64080af3c2be7579
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-07 22:07:45 +0100

    Table

--- login.php
+++ login.php
@@ -95,17 +95,23 @@
       $serverName = "tcp:sznoh.database.windows.net,1433";
       $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-      $sql="SELECT * FROM Platnosci_SZNOH";
+      /*$sql="SELECT * FROM Platnosci_SZNOH";
       $stmt = sqlsrv_query( $conn, $sql );
       sqlsrv_fetch( $stmt );
       $name = sqlsrv_get_field( $stmt, 3 );
-      echo "$name: ";
-      #$result=$conn->query('SELECT * FROM Platnosci_SZNOH');
-      #$cols = $result->columnCount();
-      #echo 'Number of returned columns: '. $cols. '<br />';
-      echo "Helloo World!";
+      echo "$name: ";*/
 
-      echo "New world!New php";
+
+      $sth = $dbh->prepare("SELECT * FROM Platnosci_SZNOH");
+      $sth->execute();
+      /* Fetch all of the remaining rows in the result set */
+      print("Fetch all of the remaining rows in the result set:\n");
+      $result = $sth->fetchAll();
+
+
+      #echo "Helloo World!";
+
+      #echo "New world!New php";
 
 
       ?>
