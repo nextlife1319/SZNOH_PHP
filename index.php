9da723b000ae81f52bbc2130c7729ba96f6b76c6
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-07 23:51:53 +0100

    r

--- index.php
+++ index.php
@@ -63,10 +63,10 @@ function newSite(string) {
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/index.php">Home</a></li>
+        <li class="active"><a class="btn btn-dark" href="/index.php">Home</a></li>
         <li><input type="button" value="Table" onClick="newSite(`/Table.php`)" class="btn btn-dark" /></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a class="btn btn-dark" href="#">Projects</a></li>
+        <li><a class="btn btn-dark" href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
