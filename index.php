aa9c1de41c79b887ae3a0caaf633be41c9097923
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-08 00:02:15 +0100

    fdghfg

--- index.php
+++ index.php
@@ -64,9 +64,9 @@ function newSite(string) {
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a class="btn btn-dark" href="/index.php">Home</a></li>
-        <li><a class="btn btn-dark" onClick="newSite(`/Table.php`)">Tabela1</a></li>
-        <li><a class="btn btn-dark" onClick="newSite(`/Table.php`)">Tabela2</a></li>
-        <li><a class="btn btn-dark" onClick="newSite(`/Table.php`)">Tabela3</a></li>
+        <li><a class="btn btn-dark" onClick="newSite(`/Table1.php`)">Tabela1</a></li>
+        <li><a class="btn btn-dark" onClick="newSite(`/Table1.php`)">Tabela2</a></li>
+        <li><a class="btn btn-dark" onClick="newSite(`/Table1.php`)">Tabela3</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
@@ -84,47 +84,6 @@ function newSite(string) {
     </div>
     <div class="col-sm-8 text-left">
       <iframe id="myFrame" src="/Home.html" style="height:1000px;width:100%" frameBorder="0"></iframe>
-      <?php/*
-      // PHP Data Objects(PDO) Sample Code:
-      try {
-          $conn = new PDO("sqlsrv:server = tcp:sznoh.database.windows.net,1433; Database = SZNOH_DB", "ServerAdmin", "WCYwcy123");
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-      catch (PDOException $e) {
-          print("Error connecting to SQL Server.");
-          die(print_r($e));
-      }
-
-      // SQL Server Extension Sample Code:
-      $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Database" => "SZNOH_DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-      $serverName = "tcp:sznoh.database.windows.net,1433";
-      $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-      $sql="SELECT * FROM Platnosci_SZNOH";
-      $stmt = sqlsrv_query( $conn, $sql );
-
-      ?>
-      <table border="1" style="width:100%">
-        <tr BGCOLOR="#6D8FFF">
-            <td>ID</td>
-            <td>Name</td>
-            <td>Age</td>
-            <td>Price</td>
-        </tr>
-      <?php
-      while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-      ?>
-        <tr>
-          <td><?php echo $row[0]; ?></td>
-          <td><?php echo $row[1]; ?></td>
-          <td><?php echo $row[2]; ?></td>
-          <td><?php echo $row[3]; ?></td>
-        </tr>
-      <?php
-    }*/
-      ?>
-      <?php//</table>?>
-
     </div>
     <div class="col-sm-2 sidenav">
       <div class="well">
