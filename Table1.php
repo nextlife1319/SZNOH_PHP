68630d0cef7e450aca9132a8e74a5f45fc9eafbd
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-08 00:23:04 +0100

    Tables

--- Table1.php
+++ Table1.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -7,50 +8,52 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <h1>Pierwsza Tabela</h1>
-    <?php
-    // PHP Data Objects(PDO) Sample Code:
-    try {
-        $conn = new PDO("sqlsrv:server = tcp:sznoh.database.windows.net,1433; Database = SZNOH_DB", "ServerAdmin", "WCYwcy123");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+    <div class="container">
+      <h1>Pierwsza Tabela</h1>
+      <?php
+      // PHP Data Objects(PDO) Sample Code:
+      try {
+          $conn = new PDO("sqlsrv:server = tcp:sznoh.database.windows.net,1433; Database = SZNOH_DB", "ServerAdmin", "WCYwcy123");
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $e) {
+          print("Error connecting to SQL Server.");
+          die(print_r($e));
+      }
 
-    // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Database" => "SZNOH_DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:sznoh.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+      // SQL Server Extension Sample Code:
+      $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Database" => "SZNOH_DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+      $serverName = "tcp:sznoh.database.windows.net,1433";
+      $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-    $sql="SELECT * FROM Platnosci_SZNOH";
-    $stmt = sqlsrv_query( $conn, $sql );
+      $sql="SELECT * FROM Platnosci_SZNOH";
+      $stmt = sqlsrv_query( $conn, $sql );
 
-    ?>
-    <table class="table" style="width:100%">
-      <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Price</th>
-        </tr>
-      </thead>
-    <?php
-    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-    ?>
-      <tbody>
-        <tr>
-          <td><?php echo $row[0]; ?></td>
-          <td><?php echo $row[1]; ?></td>
-          <td><?php echo $row[2]; ?></td>
-          <td><?php echo $row[3]; ?></td>
-        </tr>
-      </tbody>
-    <?php
-    }
-    ?>
-    </table>
+      ?>
+      <table class="table" style="width:100%">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Price</th>
+          </tr>
+        </thead>
+      <?php
+      while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
+      ?>
+        <tbody>
+          <tr>
+            <td><?php echo $row[0]; ?></td>
+            <td><?php echo $row[1]; ?></td>
+            <td><?php echo $row[2]; ?></td>
+            <td><?php echo $row[3]; ?></td>
+          </tr>
+        </tbody>
+      <?php
+      }
+      ?>
+      </table>
+    </div>
   </body>
 </html>
