e63aa01b102cf120cc9f26bb71417079eb079edf
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-09 20:42:39 +0100

    a

--- update-single.php
+++ update-single.php
@@ -8,10 +8,25 @@
 
  require_once('Table1.php');
 
+ if (isset($_POST['Submit'])) {
+   echo "dupa";
+
+   $rowToUpdate = $_POST['ID'];
+   $tsql= "UPDATE Platnosci_SZNOH SET IDPlatnosci = ?, Zaplacono = ?, Zaliczka = ?, CalaKwota = ?  WHERE IDPlatnosci = ?";
+   $params = array($_POST['ID'], $_POST['Zap'], $_POST['Zal'], $_POST['Cal'], $rowToUpdate);
+
+   $getResults= sqlsrv_query($conn, $tsql, $params);
+   $rowsAffected = sqlsrv_rows_affected($getResults);
+   if ($getResults == FALSE or $rowsAffected == FALSE)
+       die(FormatErrors(sqlsrv_errors()));
+   echo ($rowsAffected. " row(s) updated: " . PHP_EOL);
+   sqlsrv_free_stmt($getResults);
+ }
+
 $row=find_row_by_id($_GET['id']);
 ?>
 
-<form>
+<form method="post">
   IDPlatnosci:<br>
   <input type="text" name="ID" value="<?php echo $row[0]; ?>"><br>
   Zaplacono:<br>
@@ -22,21 +37,3 @@ $row=find_row_by_id($_GET['id']);
   <input type="text" name="Cal" value="<?php echo $row[3]; ?>"><br>
   <input type="submit" value="submit">
 </form>
-
-<?php
-
-if (isset($_POST['Submit'])) {
-  echo "dupa";
-
-  $rowToUpdate = $_POST['ID'];
-  $tsql= "UPDATE Platnosci_SZNOH SET IDPlatnosci = ?, Zaplacono = ?, Zaliczka = ?, CalaKwota = ?  WHERE IDPlatnosci = ?";
-  $params = array($_POST['ID'], $_POST['Zap'], $_POST['Zal'], $_POST['Cal'], $rowToUpdate);
-
-  $getResults= sqlsrv_query($conn, $tsql, $params);
-  $rowsAffected = sqlsrv_rows_affected($getResults);
-  if ($getResults == FALSE or $rowsAffected == FALSE)
-      die(FormatErrors(sqlsrv_errors()));
-  echo ($rowsAffected. " row(s) updated: " . PHP_EOL);
-  sqlsrv_free_stmt($getResults);
-}
-?>
