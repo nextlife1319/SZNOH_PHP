3c6d9a0e43ddf516f4e2c3a6f64f3c980b168aab
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-09 23:49:53 +0100

    a

--- Table.php
+++ Table.php
@@ -14,7 +14,8 @@
       echo __DIR__;
       include __DIR__.'/../functions.php';
       $nazwaTabeli="Platnosci_SZNOH";
-      echo display_table($nazwaTabeli);
+      $var=display_table($nazwaTabeli);
+      echo $var;
       // SQL Server Extension Sample Code:
       $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Database" => "SZNOH_DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
       $serverName = "tcp:sznoh.database.windows.net,1433";
