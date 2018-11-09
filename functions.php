440e3d175149ca60cb9b465c0e044c8efe20e7c6
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-10 00:07:26 +0100

    a

--- functions.php
+++ functions.php
@@ -3,30 +3,30 @@
 
 
 function display_table($nazwaTabeli, $conn){
-// $var="dziala";
-// return $var;
-// }
-
-$sql="SELECT * FROM $nazwaTabeli";
-$stmt = sqlsrv_query( $conn, $sql );
-
-
-  echo "<table class="table table-hover">";
-    echo "<thead>";
-      echo "<tr>";
-        foreach( sqlsrv_field_metadata( $stmt ) as $fieldMetadata ) {
-                $col=$fieldMetadata["Name"];
-                echo "<th>".$col."</th>";
-              }
-              echo "<th>Edit</th></tr></thead><tbody>";
-    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-
-      echo "<tr>";
-      foreach($row as $element){
-        echo  "<td>".$element."</td>";
-      }
-      echo "<td><a href=update-single.php?id=".$row[0].">Edit</a></td></tr></tbody>";
-    }
-  echo "</table>";
+$var="dziala";
+echo $var;
 }
+
+// $sql="SELECT * FROM $nazwaTabeli";
+// $stmt = sqlsrv_query( $conn, $sql );
+//
+//
+//   echo "<table class="table table-hover">";
+//     echo "<thead>";
+//       echo "<tr>";
+//         foreach( sqlsrv_field_metadata( $stmt ) as $fieldMetadata ) {
+//                 $col=$fieldMetadata["Name"];
+//                 echo "<th>".$col."</th>";
+//               }
+//               echo "<th>Edit</th></tr></thead><tbody>";
+//     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
+//
+//       echo "<tr>";
+//       foreach($row as $element){
+//         echo  "<td>".$element."</td>";
+//       }
+//       echo "<td><a href=update-single.php?id=".$row[0].">Edit</a></td></tr></tbody>";
+//     }
+//   echo "</table>";
+// }
 ?>
