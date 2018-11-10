28d2c2806139b56cacfa4533842d2c0d7d3a45ec
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-10 01:49:45 +0100

    a

--- update-single.php
+++ update-single.php
@@ -47,10 +47,8 @@ $row=find_row_by_id($_GET['id'], $conn);
   CalaKwota:<br>
   <input type="text" name="Cal" value="<?php echo $row[3]; ?>"><br><br>
   <input type="submit" value="Wprowadź">
-  <form action="/Table.php">
-    <input type="submit" value="Wróć" />
-  </form>
+  <a href="/Table.php">Wróć</a>
 </form>
 </div>
-<a class="btn btn-dark" href="/Table.php">Wróć</a>
+<a href="/Table.php">Wróć</a>
 </html>
