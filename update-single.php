4d601c21223b178d039b29cc4e63825f1cecf89d
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-10 01:30:44 +0100

    a

--- update-single.php
+++ update-single.php
@@ -37,5 +37,5 @@ $row=find_row_by_id($_GET['id'], $conn);
   CalaKwota:<br>
   <input type="text" name="Cal" value="<?php echo $row[3]; ?>"><br><br>
   <input type="submit" value="Wprowadź">
-  <button onclick="history.back();">Wróć</button>
+  <button onclick="newSite(`/Table.php`)">Wróć</button>
 </form>
