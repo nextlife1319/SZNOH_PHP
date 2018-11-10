131db88bc363a87325fd4c236500f296d3e7c5d0
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-10 13:15:12 +0100

    a

--- Table.php
+++ Table.php
@@ -16,7 +16,7 @@
       $connectionInfo = array("UID" => "SecureAdmin@sznohfal", "pwd" => "{WCYwcy123}", "Database" => "sznohphp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
       $serverName = "tcp:sznohfal.database.windows.net,1433";
       $conn = sqlsrv_connect($serverName, $connectionInfo)
-
+      echo "jestem";
       $nazwaTabeli="Klienci";
       display_table($nazwaTabeli,$conn);
       echo "<a class='btn btn-primary' href='/add-single.php'>Dodaj</a>";
