9f7bf1f9e546ad4cd34bdb89d64c528d825c2510
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-11-16 00:29:48 +0100

    login

--- login.php
+++ login.php
@@ -1,131 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>SZNOH System zarzadzania nowoczesnym obiektem hotelowym</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
-
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height:auto;}
-    }
-  </style>
-</head>
-<body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<div class="container-fluid text-center">
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-    </div>
-    <div class="col-sm-8 text-left">
-      <h1>Welcome</h1>
-      <iframe src="test.html">
-        <p>No support</p>
-      </iframe>
-      <?php
-      // PHP Data Objects(PDO) Sample Code:
-      try {
-          $conn = new PDO("sqlsrv:server = tcp:sznoh.database.windows.net,1433; Database = SZNOH_DB", "ServerAdmin", "WCYwcy123");
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-      catch (PDOException $e) {
-          print("Error connecting to SQL Server.");
-          die(print_r($e));
-      }
-
-      // SQL Server Extension Sample Code:
-      $connectionInfo = array("UID" => "ServerAdmin@sznoh", "pwd" => "WCYwcy123", "Database" => "SZNOH_DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-      $serverName = "tcp:sznoh.database.windows.net,1433";
-      $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-      /*$sql="SELECT * FROM Platnosci_SZNOH";
-      $stmt = sqlsrv_query( $conn, $sql );
-      sqlsrv_fetch( $stmt );
-      $name = sqlsrv_get_field( $stmt, 3 );
-      echo "$name: ";*/
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <h1>Zaloguj</h1>
+<?php
+ require_once('functions.php');
+ $connectionInfo = array("UID" => "SecureAdmin@sznohfal", "pwd" => "WCYwcy123", "Database" => "sznohphp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+ $serverName = "tcp:sznohfal.database.windows.net,1433";
+ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
-      #echo "Helloo World!";
+ if (isset($_POST[$col[1]])) {
 
-      #echo "New world!New php";
+   $addsql= "INSERT INTO ".$nazwaTabeli." (";
+   $val=") VALUES (";
+   $ii=0;
+   foreach($col as $element){
+     if ($element!=$col[0]){
+       $addsql.=$element;
+       $addsql.=", ";
+       $params[]=$_POST[$col[$ii]];
+       $val.="?, ";
+     }
+     $ii=$ii+1;
+   }
+   $addsql=rtrim($addsql, ", ");
+   $val=rtrim($val, ", ");
+   $addsql.=$val.");";
 
+   $getResults= sqlsrv_query($conn, $addsql, $params);
+   $rowsAffected = sqlsrv_rows_affected($getResults);
+   echo "Dodano wpis";
+   sqlsrv_free_stmt($getResults);
+ }
 
-      ?>
-      <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
-    </div>
-    <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>ADS</p>
-      </div>
-      <div class="well">
-        <p>ADS</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<footer class="container-fluid text-center">
-  <p>Made by talented WAT students</p>
-</footer>
-
-</body>
-</html>
+echo "<div class='input-group mb-3'><form method='post'>";
+echo 'username'.":<br>";
+echo "<input type='text' name='user'><br>";
+echo "Password:<br>";
+echo "<input type='text' name='password'><br>";
+echo "<input class='btn btn-primary' type='submit' value='Wprowadź'><a class='btn btn-primary' href='/login.php'>Wróć</a></form></div></body></html>";
+?>
