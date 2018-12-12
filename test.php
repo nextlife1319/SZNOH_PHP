951ea8ec10999d13d23044c2156f2686ed918023
root <root@localhost.localdomain>
2018-12-12 11:16:00 -0500

    Calendar

--- test.php
+++ test.php
@@ -6,6 +6,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- FOR CALENDAR -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
+
   </head>
   <body>
     <div class="container">
@@ -38,6 +51,9 @@
       $name=$_COOKIE['name'];   #obtaining name
       echo "<br>".$name;
 ?>
+<br>
+<p>Date: <input type="text" id="datepicker"></p>
+
     </div>
   </body>
 </html>
