57e32d929456d8176a9a97367e862c9012bd213d
root <root@localhost.localdomain>
2018-12-12 07:57:45 -0500

    a

--- index.php
+++ index.php
@@ -62,7 +62,11 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
+
         <li><a class="btn btn-dark" href="/index.php">Home</a></li>
+        <?php
+              if(isset($_COOKIE['admin'])) {
+                echo """
         <li><a class="btn btn-dark" onClick="newSite(`/Pokoje.php`)">Pokoje</a></li>
         <li><a class="btn btn-dark" onClick="newSite(`/Pracownicy.php`)">Pracownicy</a></li>
         <li><a class="btn btn-dark" onClick="newSite(`/Klienci.php`)">Klienci</a></li>
@@ -70,6 +74,8 @@
         <li><a class="btn btn-dark" onClick="newSite(`/Wyposazenie.php`)">Wyposazenie</a></li>
         <li><a class="btn btn-dark" onClick="newSite(`/Spis_wyposazenia.php`)">Spis wyposazenia</a></li>
         <li><a class="btn btn-dark" onClick="newSite(`/Wiadomosci.php`)">Wiadomosc</a></li>
+        """
+        ?>
         <!--<li><a class="btn btn-dark" onClick="newSite(`/users.php`)">Uzytkownicy</a></li>-->
       </ul>
       <ul class="nav navbar-nav navbar-right">
