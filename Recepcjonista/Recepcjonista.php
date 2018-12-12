44a272875d35dceec3dd559ea8a488d75e8b73cc
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-12-12 19:38:15 +0100

    a

--- Recepcjonista/Recepcjonista.php
+++ Recepcjonista/Recepcjonista.php
@@ -24,11 +24,11 @@
       $name=explode(" ", $_COOKIE['name']);
       print_r($name);
 
-      $sql="SELECT * FROM $nazwaTabeli WHERE imie=$name[0] AND nazwisko=$name[1]";
-      $stmt = sqlsrv_query( $conn, $sql );
-
-      $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC)
-      print_r($row);
+      // $sql="SELECT * FROM $nazwaTabeli WHERE imie=$name[0] AND nazwisko=$name[1]";
+      // $stmt = sqlsrv_query( $conn, $sql );
+      //
+      // $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC)
+      // print_r($row);
         // echo "<tr>";
         // foreach($row as $element){
         //   echo  "<td>".$element."</td>";
