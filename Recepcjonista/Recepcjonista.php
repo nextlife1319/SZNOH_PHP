d3c9398ce5688a91bd76f82e5123005fefac3df3
Reynaven <38563844+Reynaven@users.noreply.github.com>
2018-12-12 19:06:22 +0100

    a

--- Recepcjonista/Recepcjonista.php
+++ Recepcjonista/Recepcjonista.php
@@ -9,21 +9,28 @@
   </head>
   <body>
     <div class="container">
-      <?php $nazwaTabeli="users"; ?>
+      <?php $nazwaTabeli="Pracownicy"; ?>
       <h1><?php echo $nazwaTabeli; ?></h1>
       <?php
       $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-      echo $root;
       require_once($root.'/functions.php');
 
       $connectionInfo = array("UID" => "SecureAdmin@sznohfal", "pwd" => "WCYwcy123", "Database" => "sznohphp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
       $serverName = "tcp:sznohfal.database.windows.net,1433";
       $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-      display_table($nazwaTabeli,$conn);
-      echo "<a class='btn btn-primary' href=/add-single.php?nazwaTabeli=".$nazwaTabeli.">Dodaj</a>";
+      $col=get_col_names($nazwaTabeli, $conn);
 
-?>
+      echo "<div class='input-group mb-3'><form method='post'>";
+
+      foreach($col as $element){
+        if ($element!=$col[0]){
+          echo $element.":<br>";
+          echo "<input type='text' name='".$element."'><br>";
+        }
+      }
+      echo "<input class='btn btn-primary' type='submit' value='Wprowadź'><a class='btn btn-primary' href='/".$nazwaTabeli.".php'>Wróć</a></form></div></body></html>";
+      ?>
     </div>
   </body>
 </html>
