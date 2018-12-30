4e5372f44d90ccbd4b9d908733a67f0140797695
Reynaven <Reynaven@users.noreply.github.com>
2018-12-30 15:04:42 +0100

    a

--- Recepcjonista/menu.php
+++ Recepcjonista/menu.php
@@ -15,6 +15,15 @@
     <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/show-history.php '">Zobacz historie</button>
     <button type="button" class="btn btn-primary btn-lg btn-block">Sprawdz rozmieszczenie klientow</button>
 
-
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/add-single-client.php?nazwaTabeli=Klienci'">Dodaj klienta</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/add-reservation.php '">Wprowadź nowa rezerwacje</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Tabele/Rezerwacje.php '">Wyświetl liste rezerwacji</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/show-history.php '">Zobacz historie</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block">Sprawdz rozmieszczenie klientow</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/add-single-client.php?nazwaTabeli=Klienci'">Dodaj klienta</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/add-reservation.php '">Wprowadź nowa rezerwacje</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Tabele/Rezerwacje.php '">Wyświetl liste rezerwacji</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='/Recepcjonista/show-history.php '">Zobacz historie</button>
+    <button type="button" class="btn btn-primary btn-lg btn-block">Sprawdz rozmieszczenie klientow</button>
 
   </body>
