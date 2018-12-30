0a50943629b071bf19eed27ad6939f2497e63767
Reynaven <Reynaven@users.noreply.github.com>
2018-12-30 16:40:54 +0100

    a

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ EOD;
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-2 sidenav">
-      <h1>Powiadomienia:</h1>
+      <h3>Powiadomienia:</h3>
       <p> Uwaga, nie tworzyć rezerwacji między 12.12.2018 a 31.12.2018 </p>
       <p> Za trzy dni upłynie termina ważności hasła. Proszę o zmiane. </p>
       <?php //<p><a href="#">Link</a></p> ?>
