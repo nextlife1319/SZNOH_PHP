6debc203b5c55737f66025d29ce7e864286a00aa
Reynaven <Reynaven@users.noreply.github.com>
2018-12-30 11:40:37 +0100

    a

--- Recepcjonista/add-reservation.php
+++ Recepcjonista/add-reservation.php
@@ -35,10 +35,11 @@
 
     if (isset($_POST["id_pracownika"])) {
       echo "kupa";
+
+      $addsql= 'INSERT INTO Rezerwacje (id_pokoju, id_klienta, id_pracownika, data_przyjazdu, data_wyjazdu, czy_zameldowany) VALUES ';
+      $addsql+='('.$_POST["id_pokoju").', '.$_POST["id_klienta").', '.$_POST["id_pracownika").', '.$_POST["data_przyjazdu").', '.$_POST["data_wyjazdu").', 1);';
+      echo $addsql;
     }
-    //   $addsql= 'INSERT INTO Rezerwacje (id_pokoju, id_klienta, id_pracownika, data_przyjazdu, data_wyjazdu, czy_zameldowany) VALUES ';
-    //   $addsql+='('.$_POST["id_pokoju").', '.$_POST["id_klienta").', '.$_POST["id_pracownika").', '.$_POST["data_przyjazdu").', '.$_POST["data_wyjazdu").', 1);';
-    //
     //   $getResults= sqlsrv_query($conn, $addsql);
     //   $rowsAffected = sqlsrv_rows_affected($getResults);
     //   echo "Dodano wpis";
