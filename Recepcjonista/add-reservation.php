c900637224054dd7e6fdf3b45591b70380c2d200
Reynaven <Reynaven@users.noreply.github.com>
2018-12-30 11:30:04 +0100

    a

--- Recepcjonista/add-reservation.php
+++ Recepcjonista/add-reservation.php
@@ -25,11 +25,25 @@
     </script>
       <!-- END FOR CALENDAR -->
 
-
-
   </head>
   <body>
+    <?php
 
+    $connectionInfo = array("UID" => "SecureAdmin@sznohfal", "pwd" => "WCYwcy123", "Database" => "sznohphp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:sznohfal.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    if (isset($_POST["id_pracownika")) {
+
+      $addsql= 'INSERT INTO Rezerwacje (id_pokoju, id_klienta, id_pracownika, data_przyjazdu, data_wyjazdu, czy_zameldowany) VALUES ';
+      $addsql+='('.$_POST["id_pokoju").', '.$_POST["id_klienta").', '.$_POST["id_pracownika").', '.$_POST["data_przyjazdu").', '.$_POST["data_wyjazdu").', 1);';
+
+      $getResults= sqlsrv_query($conn, $addsql);
+      $rowsAffected = sqlsrv_rows_affected($getResults);
+      echo "Dodano wpis";
+      sqlsrv_free_stmt($getResults);
+    }
+    ?>
 
 <div class='input-group mb-3'><form method='post'>
     <?php
@@ -38,7 +52,7 @@
 
     echo "<br>Klient<br>";
     $clients = getClients();    #Client dropdown
-    echo '<select name="ID_client">';
+    echo '<select name="id_klienta">';
     foreach($clients as $row)
     {
       $tmparr=explode("|",$row);
@@ -48,7 +62,7 @@
 
     echo "<br>Pokój<br>";
     $rooms = getRooms();        #Room dropdown
-    echo '<select name="ID_room">';
+    echo '<select name="id_pokoju">';
     foreach($rooms as $row)
     {
       $tmparr=explode("|",$row);
@@ -58,7 +72,7 @@
 
     echo getIdFromName($_COOKIE['name']);
 
-    echo '<input type="hidden" name="ID_prac" value='.getIdFromName($_COOKIE['name']).'>';
+    echo '<input type="hidden" name="id_pracownika" value='.getIdFromName($_COOKIE['name']).'>';
 
     echo "<br>Data przyjazdu<br>";
     echo '<input type="text" id="datepicker" name="data_przyjazdu">';
